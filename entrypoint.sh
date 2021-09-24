d62a858d1d927cbaa999328df7e5e9ac7f9ce3e9
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2021-09-24 19:05:05 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@
 
 sed -e "s/\${RANCHER_URL}/$RANCHER_URL/" -e "s/\${RANCHER_TOKEN}/$RANCHER_TOKEN/" /runner/config_template > /runner/.kube/config
 
-kubectl rollout restart deployment/$1 -n $2
+res=$(kubectl rollout restart deployment/$1 -n $2)
+
+echo "::set-output name=res::$res"
 
 rm -f /runner/.kube/config

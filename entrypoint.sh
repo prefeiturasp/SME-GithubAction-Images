e6f2b40a5794ccd76a788b7012e6e65b210b9da5
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2021-09-25 02:13:48 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,12 @@
 
 set -e
 
-#$1 = deployment name / ${INPUT_DEPLOYMENT}
-#$2 = namespace / ${INPUT_NAMESPACE}
-
-
-echo ${RANCHER_URL}
-
 sed -e "s/\${RANCHER_URL}/${RANCHER_URL}/" -e "s/\${RANCHER_TOKEN}/${RANCHER_TOKEN}/" /runner/config_template > /runner/kube.yaml
 
 cat /runner/kube.yaml
 
 export KUBECONFIG=/runner/kube.yaml
-env
-echo ${INPUT_DEPLOYMENT}
-echo ${INPUT_NAMESPACE}
-echo $HOME
-echo ${HOME}
-echo $KUBECONFIG
-echo ${KUBECONFIG}
 
-kubectl rollout restart deployment/$1 -n $2
-
-#res=$(kubectl rollout restart deployment/$1 -n $2)
-
-#echo "::set-output name=res::$res"
+kubectl rollout restart deployment/${WORKLOAD} -n ${NAMESPACE}
 
 rm -f /runner/kube.yaml

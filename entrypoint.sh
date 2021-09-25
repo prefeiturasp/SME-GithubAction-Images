d4f79158e75100fd742708f1d26439f2bc859e9a
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2021-09-25 01:33:05 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,11 @@ echo ${RANCHER_URL}
 sed -e "s/\${RANCHER_URL}/${RANCHER_URL}/" -e "s/\${RANCHER_TOKEN}/${RANCHER_TOKEN}/" /runner/config_template > /runner/.kube/config
 
 cat /runner/.kube/config
-
+env
 echo ${INPUT_DEPLOYMENT}
 echo ${INPUT_NAMESPACE}
-
+echo $HOME
+echo ${HOME)
 kubectl rollout restart deployment/$1 -n $2
 
 #res=$(kubectl rollout restart deployment/$1 -n $2)

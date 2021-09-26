a320212df50d8e5827a5fc7320fb6c7c11349c6c
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2021-09-26 13:31:05 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,6 @@
 
 set -e
 
-sed -e "s/\${RANCHER_URL}/${RANCHER_URL}/" -e "s/\${RANCHER_TOKEN}/${RANCHER_TOKEN}/" /runner/config_template > /runner/kube.yaml
+sh -c ${INPU_ARGCMD}
 
-cat /runner/kube.yaml
-
-export KUBECONFIG=/runner/kube.yaml
-
-if [ -z "$WORKLOAD" ]; then
-    kubectl -n ${NAMESPACE} rollout restart deploy
-    exit 0
-fi
-
-kubectl rollout restart deployment/${WORKLOAD} -n ${NAMESPACE}
-
-rm -f /runner/kube.yaml
+sh -c ${ENVCMD}

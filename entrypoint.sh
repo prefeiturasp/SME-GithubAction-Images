42e562af167addab697a8fe3e40d213b896bc718
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2021-09-27 19:19:56 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,11 @@ set -e
 begin_cmd="/runner/dotnet-sonarscanner begin \\
     /k:\"${SONAR_PROJECT_KEY//[$'\t\r\n']:?Please set the projectKey environment variable.}\" \\
     /d:sonar.host=\"${SONAR_HOST:?Please set the SONAR_HOST environment variable.}\" \\
-    /d:sonar.login=\"${SONAR_TOKEN:?Please set the SONAR_TOKEN environment variable.}\""
+    /d:sonar.login=\"${SONAR_TOKEN:?Please set the SONAR_TOKEN environment variable.}\" "
 
 
 end_cmd="/runner/dotnet-sonarscanner end \\
-     /d:sonar.login=\"${SONAR_TOKEN:?Please set the SONAR_TOKEN environment variable.}\""
+     /d:sonar.login=\"${SONAR_TOKEN:?Please set the SONAR_TOKEN environment variable.}\" "
 
 #if string exists and is not empty.
 if [ -n $SONAR_EXTRA_ARG ]
